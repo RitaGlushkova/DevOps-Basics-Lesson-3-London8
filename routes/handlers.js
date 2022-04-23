48669b0734b70e35316210a83920bfdd8914afcd
Margarita Glushkova <m.glushkova1@gmail.com>
2022-04-23 14:14:01 +0100

    Logs for name and number

--- routes/handlers.js
+++ routes/handlers.js
@@ -3,13 +3,13 @@ let request_number = 0;
 const indexHandler = (req, res) => {
   var current_time = new Date();
   request_number += 1;
-
   res.send(`Hello! The current server time in is ${current_time}. The server has responded to ${request_number} requests since it started.`)
 }
 
 const sayHelloHandler = (req, res) => {
+  var current_time = new Date();
   const name = req.params.name || "Friend";
-
+console.log(`[${current_time}] says hello to ${name}`)
   res.send(`Hello ${name}!`);
 }
 
